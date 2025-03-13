a7fcfa694dc921e9c30acdcbc835913a6b44d9fa
Souissi98 <pistoler95@gmail.com>
2025-03-14 00:02:52 +0100

    Designfix

--- src/components/home/Carousel.tsx
+++ src/components/home/Carousel.tsx
@@ -1,50 +1,61 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore from 'swiper';
-import 'swiper/css';
-import './Carousel.css';
+// Import des modules
 import { Autoplay, FreeMode } from 'swiper/modules';
 
-
-// Import Swiper styles
+// Import obligatoire des CSS
 import 'swiper/css';
+import 'swiper/css/autoplay';
 import 'swiper/css/free-mode';
 
-SwiperCore.use([Autoplay, FreeMode]);
+import './Carousel.css';
 
-const Carousel = ({ slides }: { slides: any[] }) => {
+interface SlideItem {
+  image: string;
+  name?: string;
+}
+
+const Carousel = ({ slides }: { slides: SlideItem[] }) => {
   return (
     <div className="u-wrapper">
       <div className="c-carousel">
+        {slides ? (
         <Swiper
-          spaceBetween={0}
-          speed={3000}
+          modules={[Autoplay, FreeMode]}
+          slidesPerView={1}
+          freeMode={true}
+          freeModeMomentum={false}
+          loop={true}
           autoplay={{
             delay: 0,
             disableOnInteraction: false,
+            pauseOnMouseEnter: false,
           }}
-          loop={true}
-          freeMode={false}
-          freeModeMomentum={false}
+          speed={4000}
           allowTouchMove={true}
-          loopAdditionalSlides={slides ? slides.length : 0}
-          slidesPerView={1}
+          spaceBetween={0}
           breakpoints={{
-            375: { slidesPerView: 2 },
-            640: { slidesPerView: 3 },
-            768: { slidesPerView: 3 },
+            375:  { slidesPerView: 2 },
+            640:  { slidesPerView: 3 },
+            768:  { slidesPerView: 3 },
             1024: { slidesPerView: 5 },
           }}
+
           className="c-carousel__wrapper"
         >
           {slides?.map((slide, index) => (
             <SwiperSlide key={index} className="c-carousel__item">
-              <img 
-                  src={`${process.env.GATSBY_API_URL}${slide?.image}`}              
-              alt={slide.name} className="h-20" />
+              <img
+                src={`${process.env.GATSBY_API_URL}${slide.image}`}
+                alt={slide.name || ''}
+                className="h-20"
+              />
             </SwiperSlide>
           ))}
         </Swiper>
+      ) : (
+          <p>Loading...</p>
+        )}
       </div>
     </div>
   );
