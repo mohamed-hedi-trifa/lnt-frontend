bfa7b864b5071ee049307e9225ad89d15401e05e
Souissi98 <pistoler95@gmail.com>
2025-04-20 00:21:02 +0100

    b

--- src/components/visitor/news/CardRelatedBlog.tsx
+++ src/components/visitor/news/CardRelatedBlog.tsx
@@ -3,20 +3,21 @@ import React from 'react'
 
 export default function CardRelatedBlog({ imgSrc, title, date }: { imgSrc: any, title: string, date: string }) {
   return (
-    <div className='border-b border-[#183354] '>
-    <article className="flex flex-col gap-4 py-[25px] w-full ">
-      <div className="w-full lg:w-[300px] lg:h-[200px] overflow-hidden">
-        <img src={`${process.env.GATSBY_API_URL}${imgSrc}`}
-          className="rounded-xl h-full w-full shadow-[0px_4px_4px_0px_#00000040] object-cover" />
-      </div>
-      <Title
-        size="text-[20px] font-semibold leading-[32px] capitalize"
-        customClassName=""
-      >
-        {title}
-      </Title>
-      <div className="text-sm text-gray-500">{date}</div>
-    </article>
-    </div>
+    <>
+      <article className="flex flex-col gap-4 py-[25px] w-full ">
+        <div className="w-full lg:w-[300px] lg:h-[200px] overflow-hidden">
+          <img src={`${process.env.GATSBY_API_URL}${imgSrc}`}
+            className="rounded-xl h-full w-full shadow-[0px_4px_4px_0px_#00000040] object-cover" />
+        </div>
+        <Title
+          size="text-[20px] font-semibold leading-[32px] capitalize"
+          customClassName=""
+        >
+          {title}
+        </Title>
+        <div className="text-sm text-gray-500">{date}</div>
+      </article>
+
+    </>
   )
 }
--- src/components/visitor/news/RelatedNews.tsx
+++ src/components/visitor/news/RelatedNews.tsx
@@ -14,18 +14,20 @@ export default function RelatedNews({ headerName, relatedBlog, route }: { header
 
             <div className="divide-y divide-black w-full text-start max-w-[300px]">
                 {relatedBlog.map((blog: any, index: number) => (
-                    <Link to={route +  blog.slug}   >
+                    <Link to={route + blog.slug}   >
 
-                        <CardRelatedBlog
-                            key={index}
-                            imgSrc={blog.image}
-                            title={blog.title_en || blog.title_fr}
-                            date={`Le ${new Date(blog.created_at).toLocaleDateString("fr-FR", {
-                                day: "numeric",
-                                month: "long",
-                                year: "numeric",
-                            })}`}
-                        />
+                        <div className={`${index !== relatedBlog.length - 1 ? 'border-b border-[#183354]' : ''}`}>
+                            <CardRelatedBlog
+                                key={index}
+                                imgSrc={blog.image}
+                                title={blog.title_en || blog.title_fr}
+                                date={`Le ${new Date(blog.created_at).toLocaleDateString("fr-FR", {
+                                    day: "numeric",
+                                    month: "long",
+                                    year: "numeric",
+                                })}`}
+                            />
+                        </div>
                     </Link>
                 ))}
             </div>
